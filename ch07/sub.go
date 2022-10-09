850fe1d44a822ef768d78546ab31d42cc0eaf9bc
MatsuoTakuro <matsuo_takuro@retail-ai.jp>
2022-10-09 11:11:39 +0900

    return value of interface is not nil

--- ch07/sub.go
+++ ch07/sub.go
@@ -1,24 +1,22 @@
 package ch07
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func Sub() {
-	list_7_2()
+	// list_7_2()
+	list_7_3()
 }
 
 type Modem struct{}
 
-func (m *Modem) Dial() {
+func (m *Modem) Dial() {}
 
-}
+func (m *Modem) Hangup() {}
 
-func (m *Modem) Hangup() {
-
-}
-
-func (m *Modem) Sender() {
-
-}
+func (m *Modem) Sender() {}
 
 func (m *Modem) Recv() {
 	fmt.Println("Modem implements Receiver interface.")
@@ -34,4 +32,34 @@ var _ Receiver = (*Modem)(nil)
 func list_7_2() {
 	m := &Modem{}
 	m.Recv()
+}
+
+type MyErr struct{}
+
+func (me *MyErr) Error() string { return "" }
+
+var _ error = (*MyErr)(nil)
+
+func Apply() error {
+	var err *MyErr = nil
+	if err != nil {
+		return errors.New("err is not nil")
+	}
+	fmt.Println(err)
+	return err
+	// return nil
+}
+
+func Apply2() error {
+	var err error = nil
+	if err != nil {
+		return errors.New("err is not nil")
+	}
+	fmt.Println(err)
+	return nil
+}
+
+func list_7_3() {
+	fmt.Println(Apply() == nil)
+	fmt.Println(Apply2() == nil)
 }
